9690e6af7d81c3658e962f635fd8f29ccedc5841
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-01-14 17:24:28 +0100

    added navbar homepage and about section

---
+++ src/Components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './Navbar'
+import BannerBackground from '../Assets/home-banner-background.png'
+import bannerImage from '../Assets/home-banner-image.png'
+import {FiArrowRight} from "react-icons/fi"
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+        <Navbar/>
+        <div className='home-banner-container'>
+        <div className='home-bannerImage-container'>
+            <img src={BannerBackground} alt=""/>
+        </div>
+        <div className="home-text-section">
+            <h1 className='primary-heading'>
+            NetzNavi - Das ultimative Tool für Mobilfunkhändler
+            </h1>
+            <p className='primary-text'>
+            NetzNavi hebt deinen Verkaufsprozess auf ein neues Level, indem es alle Tarife  der Mobilfunkprovider vergleicht. Dank smarter Technologie erhältst du in Sekundenschnelle die besten Vertragskombinationen für deine Kunden - und maximierst dabei deine Provision.
+            </p>
+            <button className='secondary-button'>
+            Melden Sie sich jetzt an <FiArrowRight/>
+            </button>
+        </div>
+        <div className='home-image-container'>
+              <img src={bannerImage} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
