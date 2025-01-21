47a58b9c49a48945e01aa501b498dec3930b20d2
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-01-21 15:00:46 +0100

    added testimonials section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
 import Work from './Components/Work';
-
+import Testimonials from './Components/Testimonials';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Home/>
       <About/>
       <Work/>
+      <Testimonials/>
     </div>
   );
 }
