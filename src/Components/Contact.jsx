045a295782f99a7af78c865f217a9f05cffd46c9
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-01-21 15:21:45 +0100

    added contact form section spacing

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div className='contact-page-wrapper'>
         <h1 className='primary-heading'>
-            Lorem ipsrum
+        Haben Sie Fragen? 
         </h1>
-        <h1 className='primary-heading'>Haben Sie Fragen? Lassen Sie sich von uns helfen</h1>
+        <h1 className='primary-heading'>Lassen Sie sich von uns helfen</h1>
         <div className='contact-form-container'>
             <input type="text" placeholder='yourmail@gmail.com'/>
             <button className='secondary-button'>einreichen</button>
