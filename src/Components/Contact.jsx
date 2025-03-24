9ca8df785c69f9ec99a65d0b4eeb0b68e7604c29
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-03-24 12:48:30 +0100

    added email feild mandatory before sending request in contact feild

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,13 @@
-import React , { useRef } from 'react'
-import {FiArrowRight} from "react-icons/fi"
+import React, { useRef } from 'react'
+import { FiArrowRight } from "react-icons/fi"
 import emailjs from '@emailjs/browser';
+import { useState } from "react";
 
 const Contact = () => {
 
   const form = useRef();
+
+  const [messageSent, setMessageSent] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -21,28 +24,36 @@ const Contact = () => {
           console.log('FAILED...', error.text);
         },
       );
+
+    setMessageSent(true);
+    // Hide notification after 3 seconds
+    setTimeout(() => setMessageSent(false), 3000);
   };
 
 
 
   return (
     <div className='contact-page-wrapper'>
-        <h1 className='primary-heading'>
-        Haben wir ihr Interesse geweckt? 
-        </h1>
-        <h1 className='primary-heading'>Schreiben sie uns und wir kontaktieren Sie</h1>
-        <form ref={form} onSubmit={sendEmail}>
+      <h1 className='primary-heading'>
+        Haben wir ihr Interesse geweckt?
+      </h1>
+      <h1 className='primary-heading'>Schreiben sie uns und wir kontaktieren Sie</h1>
+      <form ref={form} onSubmit={sendEmail}>
         <div className='contact-form-container'>
-            <input type="text" name="user_name" placeholder='Dein Name'/>
+          <input type="text" name="user_name" placeholder='Dein Name' />
+          <input type="email" name="user_email" placeholder="Dein Email" required />
+          <input type="text" name="message" placeholder='Schreiben Sie uns hier' />
+          <button className='secondary-button' type="submit" value="Send">anfragen</button>
+          {messageSent && (
+            <div className="success-message">
+              ✅ Ihre Nachricht wurde gesendet!
+            </div>
+          )}
         </div>
-        <div className='contact-form-container'>
-            <input type="text" name="message" placeholder='Schreiben Sie uns hier'/>
-            <button className='secondary-button' type="submit" value="Send" >anfragen</button>
-        </div>
-        </form>
-        <button className='secondary-button' style={{marginTop:'5%'}} onClick={() => window.open("https://netznavi.net/", "_blank", "noopener,noreferrer")}>
-            Melden Sie sich jetzt an <FiArrowRight/>
-            </button>
+      </form>
+      <button className='secondary-button' style={{ marginTop: '5%' }} onClick={() => window.open("https://netznavi.net/", "_blank", "noopener,noreferrer")}>
+        Melden Sie sich jetzt an <FiArrowRight />
+      </button>
     </div>
   )
 }
